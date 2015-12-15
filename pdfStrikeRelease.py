c43e61f07c6079fa41865341ed9e4c54d6857782
golfecholima <george.levines@gmail.com>
2015-12-15 02:01:08 -0500

    adds raw text cleaning and pdf conversion info

--- pdfStrikeRelease.py
+++ pdfStrikeRelease.py
@@ -5,8 +5,13 @@ pdfStrikeRelease = open('/path/to/some/pdf', 'rb')
 # rb = read binary
 
 pdfReader = PyPDF2.PdfFileReader(pdfStrikeRelease)
-pdfReader.numPages
 pageObj = pdfReader.getPage(0)
-pageObj.extractText()
+rawText = pageObj.extractText()
 
 # The clean the text by removing all '\n' and adding a return where any double space occurs.
+
+import re
+
+cleanText = re.sub(r'\n', "", rawText)
+cleanText = re.sub(r'  ', "\n", cleanText)
+
