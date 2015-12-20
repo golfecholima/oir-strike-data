fbc9e1ea3e3cd10b868644f6d70dc0c1c56b3003
George <george.levines@gmail.com>
2015-12-20 17:04:45 -0500

    adds regexes for date, release # and location

--- pdfStrikeRelease.py
+++ pdfStrikeRelease.py
@@ -2,7 +2,7 @@ import PyPDF2
 
 pdfStrikeRelease = open('/path/to/some/pdf', 'rb') 
 
-# rb = read binary
+# rb stands for read binary
 
 pdfReader = PyPDF2.PdfFileReader(pdfStrikeRelease)
 pageObj = pdfReader.getPage(0)
@@ -23,10 +23,27 @@ rawText = PyPDF2.PdfFileReader(release).getPage(0).extractText()
 
 finalText = re.sub(r'  ', "\n", re.sub(r'\n', "", rawText))
 
-print (finalText)
+print(finalText)
 
 # OR
 
-print (re.sub(r'  ', "\n", re.sub(r'\n', "", PyPDF2.PdfFileReader(open('/path/to/pdf.pdf', 'rb')).getPage(0).extractText())))
+print(re.sub(r'  ', "\n", re.sub(r'\n', "", PyPDF2.PdfFileReader(open('/path/to/pdf.pdf', 'rb')).getPage(0).extractText())))
 
 # OOF
+
+### Regexes ###
+
+# For more recent files ...
+
+# Find the release number: 
+
+releaseNumber = re.findall(r'(?<=Release # )([\d]{8}-?\d?\d?)', finalText)
+
+# Find the date and give it in coding friendly format:
+
+date = re.findall(r'\w+ (\d\d?, \d\d\d?\d?)', finalText)
+
+# Find the location where the strike took place
+
+location = re.findall(r'(?<=Near )(.+?)(?=,)', finalText)
+
