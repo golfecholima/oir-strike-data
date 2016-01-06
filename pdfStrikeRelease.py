7a6dae0e49b9548adb5a1cd5e9d448feb9d6324c
George <george.levines@gmail.com>
2016-01-05 19:46:55 -0500

    adds pdfs and new py tinkering

--- pdfStrikeRelease.py
+++ pdfStrikeRelease.py
@@ -47,3 +47,39 @@ date = re.findall(r'\w+ (\d\d?, \d\d\d?\d?)', finalText)
 
 location = re.findall(r'(?<=Near )(.+?)(?=,)', finalText)
 
+if os.path.isfile(pdf_folder)
+
+
+
+***
+
+
+outputs = glob.glob('TXT/*.txt')
+inputs = glob.glob('*/*/*.pdf')
+
+
+
+for file in glob.glob('*/*/*.pdf'):
+	print 'Converting ', file, ' to .txt ...'
+
+***
+
+import os, schedule, time, PyPDF2, re
+
+tmp = '/Users/workmcgerk/Desktop/repos/oir-strike-data/tmp'
+pdf_folder = '/Users/workmcgerk/Desktop/repos/oir-strike-data/PDF/oir-strike-releases-pdf'
+txt_folder = '/Users/workmcgerk/Desktop/repos/oir-strike-data/TXT/oir-strike-releases-txt'
+
+def job():
+	for file in os.listdir(tmp):
+		print 'Converting', file,'to text ...' 
+		pdf = open(file, 'rb') # read the pdf file
+		totpages = PyPDF2.PdfFileReader(pdf).getNumPages()
+		print 'This document has', totpages, 'pages'
+		raw = PyPDF2.PdfFileReader(pdf).getPage(0).extractText() # read it with PyPDF2, get each page, extract the text
+		final = re.sub(r'  ', '\n', re.sub(r'\n', '', raw)) # do some regex to make it more readable
+		print 'Here\'s the converted text:\n', final
+		# write a file with the converted text and save it in the TXT folder
+		# move the original PDF to the PDFs folder
+
+job()
